e9e0ce894fdef77b8f1398eec8965112553acab3
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-09-11 09:55:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,69 +8,6 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
-
-const perguntas = [
-    {
-        enunciado: " Qual dos animais é um mamífero ?",
-        alternativas: [
-            {
-                texto: "Baleia ",
-                afirmacao: [
-                    "Se essa foi a apção escolhida , esta correto",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Cavalo marinho",
-                afirmacao: [
-                    "Se essa foi a opção , esta incorreta",
-                    "afirmacao 2"
-                    ]
-            }           
-            
-        ]
-    },
-    {
-        enunciado: " Qual é o unico mamífero que voa ?",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "Qual é o mamífero que bota ovo ?",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
-
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
