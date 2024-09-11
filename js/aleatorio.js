29017149a5a4844a774677418c355ee465b9c1f2
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-09-11 10:13:26 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = [ "Igor", "Aghata", "Albert", "Ariana", "Alex", "Dylan"];
 
+export funcition aleatorio (lista){
+  const posicao = math.floor(math.randon()* lista.lenght);
+  return lista(posicao);
+}
+
+export const nome = aleatorio(nome)
