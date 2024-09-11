ade35426cf0aedf0b2c0fcdf438983ac058bf98b
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-09-11 09:51:56 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,61 @@
-
+const perguntas = [
+    {
+        enunciado: " Qual dos animais é um mamífero ?",
+        alternativas: [
+            {
+                texto: "Baleia ",
+                afirmacao: [
+                    "Se essa foi a apção escolhida , esta correto",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Cavalo marinho",
+                afirmacao: [
+                    "Se essa foi a opção , esta incorreta",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: " Qual é o unico mamífero que voa ?",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Alternativa 2 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é o mamífero que bota ovo ?",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
