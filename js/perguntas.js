527ec007e16440f2d539d1a382e880d15500ca86
juju1710 <161592876+juju1710@users.noreply.github.com>
2024-09-23 09:23:30 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,14 +4,14 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Baleia ",
-                afirmacao: [
+                afirmacao 1: [
                     "Se essa foi a apção escolhida , esta correto",
                     "afirmacao 2"
                     ]
             },
             {
                 texto: "Cavalo marinho",
-                afirmacao: [
+                afirmacao 2: [
                     "Se essa foi a opção , esta incorreta",
                     "afirmacao 2"
                     ]
